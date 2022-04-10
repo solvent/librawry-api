dfeeb6feb250aae072f46c6c9adcbfe696b87d51
Maksim Gurianov <maksim.solvent@gmail.com>
2022-04-10 13:41:29 +0300

    much more better with uow and model validation

--- Controllers/TitlesController.cs
+++ Controllers/TitlesController.cs
@@ -1,43 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
-using librawry.portable.ef;
-using librawry.portable.repo;
+using librawry.portable;
 using librawry.portable.repo.titles;
-using System.Text.Json.Serialization;
 
 namespace librawry_api.Controllers {
 
 	[ApiController]
 	[Route("api/[controller]")]
 	public class TitlesController : ControllerBase {
-		private readonly LibrawryContext _context;
+		private readonly IUnitOfWork unitOfWork;
 
-		public TitlesController(LibrawryContext context) {
-			_context = context;
+		public TitlesController(IUnitOfWork unitOfWork) {
+			this.unitOfWork = unitOfWork;
 		}
 
 		[HttpGet("{id}")]
 		public async Task<ActionResult<DetailsResponse>> Details([FromRoute] int id) {
-			return await new TitleRepository(_context).GetDetails(id);
+			return await unitOfWork.TitleRepository.GetDetails(id);
 		}
 
 		[HttpPost]
-		public async Task<ActionResult<IEnumerable<ListResponse>>> List([FromBody] ListRequest requestBody) {
-			if (requestBody == null) {
-				return BadRequest();
-			}
-
-			if (string.IsNullOrEmpty(requestBody.Search) || requestBody.Search.Length < 3) {
-				return BadRequest("Please use at least 3 characters length string to search.");
-			}
-
-			return Ok(await new TitleRepository(_context).GetList(requestBody.Search));
-		}
-	}
-
-	public class ListRequest {
-		[JsonPropertyName("search")]
-		public string Search {
-			get; set;
+		public async Task<ActionResult<IEnumerable<ListResponse>>> List([FromBody] ListRequest param) {
+			return Ok(await unitOfWork.TitleRepository.GetList(param));
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using librawry.portable;
 using librawry.portable.ef;
+using librawry.portable.repo;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,7 @@ var cons = builder.Configuration.GetConnectionString("SqliteDatabase");
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<LibrawryContext>(options => options.UseSqlite(cons));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 

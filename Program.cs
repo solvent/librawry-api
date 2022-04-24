6ca0afa278eed36f651d6b86e0d8349997dc7862
Maksim Gurianov <maksim.solvent@gmail.com>
2022-04-24 14:51:07 +0300

    swagger added

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 var cons = builder.Configuration.GetConnectionString("SqliteDatabase");
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<LibrawryContext>(options => options.UseSqlite(cons));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 
 var app = builder.Build();
 
@@ -18,6 +20,8 @@ if (app.Environment.IsProduction()) {
 
 if (app.Environment.IsDevelopment()) {
 	app.UseDeveloperExceptionPage();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.MapControllers();

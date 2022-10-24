0bccad30f49db5a731c6e06678386d1c42d659cc
Maksim Gurianov <maksim.solvent@gmail.com>
2022-10-24 23:48:19 +0300

    enable nullables and net6 namespaces

--- Controllers/TitlesController.cs
+++ Controllers/TitlesController.cs
@@ -2,25 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 using librawry.portable;
 using librawry.portable.repo.titles;
 
-namespace librawry_api.Controllers {
+namespace librawry.api.Controllers;
 
-	[ApiController]
-	[Route("api/[controller]")]
-	public class TitlesController : ControllerBase {
-		private readonly IUnitOfWork unitOfWork;
+[ApiController]
+[Route("api/[controller]")]
+public class TitlesController : ControllerBase {
+	private readonly IUnitOfWork unitOfWork;
 
-		public TitlesController(IUnitOfWork unitOfWork) {
-			this.unitOfWork = unitOfWork;
+	public TitlesController(IUnitOfWork unitOfWork) {
+		this.unitOfWork = unitOfWork;
+	}
+
+	[HttpGet("{id}")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesDefaultResponseType]
+	public async Task<ActionResult<DetailsResponse>> Details([FromRoute] int id) {
+		var details = await unitOfWork.TitleRepository.GetDetails(id);
+		if (details == null) {
+			return NotFound();
 		}
+		return Ok(details);
+	}
 
-		[HttpGet("{id}")]
-		public async Task<ActionResult<DetailsResponse>> Details([FromRoute] int id) {
-			return await unitOfWork.TitleRepository.GetDetails(id);
-		}
-
-		[HttpPost]
-		public async Task<ActionResult<IEnumerable<ListResponse>>> List([FromBody] ListRequest param) {
-			return Ok(await unitOfWork.TitleRepository.GetList(param));
-		}
+	[HttpPost]
+	public async Task<ActionResult<IEnumerable<ListResponse>>> List([FromBody] ListRequest param) {
+		return Ok(await unitOfWork.TitleRepository.GetList(param));
 	}
 }
